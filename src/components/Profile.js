5bd1372b619a6754c245db454aa7e006c283ce06
sua-kim <sua1021606@gmail.com>
2021-11-10 22:55:24 +0900

    feat: 프로필 퍼블리싱

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
 import profileImage from "../assets/profileImg.jpg";
+import follower from '../assets/follower.PNG';
+import star from '../assets/star.PNG';
+import building from '../assets/building.PNG';
+import position from '../assets/position.PNG';
+import email from '../assets/mail.PNG';
+import organ1 from '../assets/organization1.PNG';
+import organ2 from '../assets/organization2.PNG';
+import organ3 from '../assets/organization3.PNG';
+import organ4 from '../assets/organization4.PNG';
 
 function Profile() {
     return (
         <ProfileWrapper>
             <ProfileImg src={profileImage} />
+            <ProfileInfo>
             <UserName>Su a Kim</UserName>
             <UserId>sua-kim</UserId>
             <Bio>🏃‍♀️ Growing Developer</Bio>
             <Edit>Edit profile</Edit>
+            <Follow>
+                <img src={follower} /><FollowerNum>7</FollowerNum> followersㆍ<FollowerNum>8</FollowerNum> followingㆍ
+                <img src={star} /><FollowerNum>5</FollowerNum>
+            </Follow>
+            <Info>
+                <img src={building} /> Ewha W. University
+            </Info>
+            <Info>
+                <img src={position} /> Seoul, Korea
+            </Info>
+            <Info>
+                <img src={email} /> sua917@ewhain.net
+            </Info>
+            <Hr />
+            <p style={{'font-weight':'600'}}>Organizations</p>
+            <Follow>
+                <img src={organ1} />
+                <img src={organ2} />
+                <img src={organ3} />
+                <img src={organ4} />
+            </Follow>
+            </ProfileInfo>
         </ProfileWrapper>
     );
 }
@@ -20,10 +52,16 @@ text-align: left;
 `
 
 const ProfileImg = styled.img`
+position: absolute;
+top: 7rem;
 text-align: center;
-width: 320px; height: 320px;
+width: 20rem; height: 20rem;
 border-radius: 50%;
 z-index: 10;
+`
+
+const ProfileInfo = styled.div`
+padding-top: 19rem;
 `
 
 const UserName = styled.div`
@@ -47,6 +85,38 @@ text-align: center;
 padding: 0.4rem;
 font-size: 0.9rem; font-weight: 600;
 cursor: pointer;
+margin-right: 2rem;
+`
+
+const Follow = styled.div`
+display: flex;
+align-items: center;
+font-size: 0.8rem;
+color: #6E7781;
+gap: 0.1rem;
+cursor: pointer;
+`
+
+const FollowerNum = styled.p`
+font-size: 0.9rem;
+font-weight: 600;
+color: black;
+margin-right: 0.2rem;
+`
+
+const Info = styled.div`
+display: flex;
+align-items: flex-start;
+font-size: 0.9rem;
+color: #24292F;
+gap: 0.1rem;
+`
+
+const Hr = styled.hr`
+width: 21rem;
+height: 0.1rem;
+background: #D8DEE4;
+border: 0;
 margin-right: 2rem;
 `
 

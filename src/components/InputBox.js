8ff3866a0e1c552febc80a02f738cc1e12390de7
LEEYUNJI <yunji965@gmail.com>
2021-11-11 15:59:10 +0900

    feat: RepoName 퍼블리싱

---
+++ src/components/InputBox.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+function InputBox() {
+
+  const [myRepo, setMyRepo] = useState({
+    Owner: '',
+    RepoName: '',
+    Description: ''
+  })
+
+  return (
+      <InputBoxWrapper>
+        <MustInputWrapper>
+          <SelectOwner>
+            <Title>Owner</Title>
+            <Input type="text" style={{width: '300px'}}></Input>
+          </SelectOwner>
+          <h1 style={{margin: '10px', fontWeight: '350', paddingTop: '35px'}}>/</h1>
+          <InputNameBox>
+            <Title>Repository name</Title>
+            <Input type="text" style={{width: '350px'}}></Input>
+          </InputNameBox>
+        </MustInputWrapper>
+        <p>Great repository names are short and memorable. Need inspiration? How about &nbsp;
+          <span style={{color:'green', fontWeight: '500'}}>cuddly-waddle?</span>
+        </p>
+        <InputTextBox>
+            <Title>Description <span style={{fontWeight: '350', fontSize: '0.9rem'}}>(optional)</span></Title>
+            <Input type="text" style={{width: '98%'}}></Input>
+          </InputTextBox>
+      </InputBoxWrapper>
+  );
+}
+
+const InputBoxWrapper = styled.div`
+  padding-bottom: 15px;
+`
+
+const MustInputWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 0px;
+  flex-wrap: wrap;
+`
+
+const SelectOwner = styled.div`
+
+`
+
+const Title = styled.h3`
+  font-weight: 500;
+  margin-bottom: 10px;
+`
+
+const Input = styled.input`
+  background-color: #f6f8fa;
+  border-radius: 6px;
+  border: 1px solid #d0d7de;
+  font-family: inherit;
+  line-height: 20px;
+  font-size: 1rem;
+  padding: 0.5rem 0rem 0.5rem 1rem; 
+`
+
+const InputNameBox = styled.div`
+
+`
+
+const InputTextBox = styled.div`
+
+`
+
+export default InputBox;
--- src/pages/CreateRepo.js
+++ src/pages/CreateRepo.js
@@ -1,22 +1,60 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from '../components/Header';
-
-const Info = styled.div`
-  height: 10vh;
-`
+import InputBox from '../components/InputBox';
 
 function CreateRepo() {
   return (
       <div>
-          <Header />
+        <CreateRepoWrapper>
           <Info>
-            <h2>Create a new repository</h2>
-            <p>A repository contains all project files, including the revision history. Already have a project repository elsewhere?</p>
-            <a href="https://github.com/new/import">Import a repository</a>
+            <p>
+              <h2 style={{fontWeight: 400, fontSize: '24px', marginBottom: '0'}}>Create a new repository</h2>
+              A repository contains all project files, including the revision history. Already have a project repository elsewhere?<br />
+              <a href="https://github.com/new/import" style={{textDecoration: 'none', color: '#0969da'}}>Import a repository.</a>
+            </p>
           </Info>
+          <RepoName>
+           <InputBox/>
+          </RepoName>
+          <Disclosure>
+
+          </Disclosure>
+          <Initialize>
+
+          </Initialize>
+        </CreateRepoWrapper>
+
       </div>
   );
 }
+
+const CreateRepoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`
+
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 8vh;
+  width: 65vw;
+  border-bottom: 1px solid #D8DEE4;
+`
+
+const RepoName = styled.div`
+  width: 65vw;
+  border-bottom: 1px solid #D8DEE4;
+`
+
+
+const Disclosure = styled.div`
+
+`
+
+const Initialize = styled.div`
+
+`
 
 export default CreateRepo;
